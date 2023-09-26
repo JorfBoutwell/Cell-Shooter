c73eed24f025e9c1bff929c2ffb6f366277e2824
Justin Nguyen <jnguyen2737@student.pps.net>
2023-09-26 10:00:06 -0700

    Updated Scene
    Added Chat Layout and Ability Cooldown Bar

--- Assets/Scenes/Justin/Waves/WaveStart.cs
+++ Assets/Scenes/Justin/Waves/WaveStart.cs
@@ -11,6 +11,10 @@ public class WaveStart : MonoBehaviour
 
     [SerializeField] TextMeshPro countdownText;
     public GameObject countdownOverlay;
+
+    public GameObject cooldownOverlayC;
+    public GameObject cooldownOverlayBarC;
+
     public TextMeshPro bulletCount;
 
     public float currentTime = 0f;
@@ -18,6 +22,9 @@ public class WaveStart : MonoBehaviour
 
     public bool startCountdown = false;
     public bool gameStart;
+
+    public bool cooldownActiveC = false;
+    float newHeight = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -38,14 +45,40 @@ public class WaveStart : MonoBehaviour
                 countdownOverlay.SetActive(true);
                 countdownText.color = Color.red;
                 countdownText.fontSize = 50;
-                countdownText.transform.position = new Vector3(9f, .5f, 0);
+                countdownText.transform.position = new Vector3(9f, 1f, 0);
             }
             if (currentTime <= 0)
             {
                 currentTime = 0;
                 startCountdown = false;
+                countdownOverlay.SetActive(false);
+                countdownText.enabled = false;
             }
         }
+
+        if(cooldownActiveC == false)
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                CooldownActive();
+                Debug.Log("hi");
+                cooldownActiveC = true;
+                
+            }
+        }
+
+        if (newHeight > 0 && cooldownActiveC)
+        {
+            Debug.Log("yo");
+            //cooldownOverlayBarC.GetComponent<RectTransform>().transform.localScale = ();
+            cooldownOverlayBarC.GetComponent<RectTransform>().sizeDelta = new Vector2(100, newHeight);
+            newHeight -= 10 * Time.deltaTime;
+        }
+        else if (newHeight <= 0)
+        {
+            cooldownActiveC = false;
+        }
+
     }
 
     private void Reset()
@@ -53,6 +86,14 @@ public class WaveStart : MonoBehaviour
         //Countdown
         currentTime = 10f;
         //EnemyDespawn
+    }
+
+    private void CooldownActive()
+    {
+        cooldownOverlayC.SetActive(true);
+        cooldownOverlayBarC.SetActive(true);
+
+        //StartCoroutine("");
     }
 
     private void StartEnemySpawn()
@@ -86,18 +127,6 @@ public class WaveStart : MonoBehaviour
         Debug.Log(currentTime);
 
         startCountdown = true;
-
-        /*for (float j = 10f; j >= 0; j -= 1 * Time.deltaTime)
-        {
-            
-            countdownText.text = j.ToString("0");
-            countdownTime -= 1;
-            Debug.Log(countdownTime);
-
-            if (countdownTime == 0) { 
-            break;
-            }
-        }*/
         
     }
 }
