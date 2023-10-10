91e0133174d438a98ccb23856a0b1f3e3d7fd928
TisyaT <ttomar6068@student.pps.net>
2023-10-10 08:51:56 -0700

    menu screen
    working on the menu/character select screen

---
+++ Assets/Tisya/Main Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        {
+
+        }
+    }
+
+    public void MatchMaking()
+
+    {
+        Debug.Log("Button pressed");
+    }
+
+}
