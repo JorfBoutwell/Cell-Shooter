a7f86d27322b767836e9bf01ad42a37509911f75
JorfBoutwell <georgeboutwell06@gmail.com>
2023-11-30 09:17:19 -0800

    Setting up char animator

--- Assets/Scenes/Tisya/Scripts/Animator.cs
+++ Assets/Scenes/Tisya/Scripts/Animator.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Animator : MonoBehaviour
 {
 
-    //public Animator animator;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
