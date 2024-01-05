5db7406f7c83836f426de06992c683b890b49e3d
JorfBoutwell <georgeboutwell06@gmail.com>
2024-01-05 10:54:01 -0800

    cont.

--- Assets/PersonalWorkspaces/Milo/Scripts/Weapon/WeaponManager.cs
+++ Assets/PersonalWorkspaces/Milo/Scripts/Weapon/WeaponManager.cs
@@ -226,18 +226,18 @@ public class WeaponManager : MonoBehaviour
                         hit.transform.gameObject.GetComponentInParent<EnemyManager>().health -= (currentWeapon.damage * 2);
                         break;
                     case 11: //teamA
-                        if(team != "A")
-                        {
+                        //if(team != "A")
+                        //{
                             Debug.Log("Team A");
                             hit.transform.gameObject.GetComponentInParent<PlayerManager>().health -= currentWeapon.damage;
-                        }
+                        //}
                         break;
                     case 13: //teamB
-                        if (team != "B")
-                        {
+                        //if (team != "B")
+                        //{
                             Debug.Log("Team B");
                             hit.transform.gameObject.GetComponentInParent<PlayerManager>().health -= currentWeapon.damage;
-                        }
+                        //}
                         break;
                     default: break;
                 }
