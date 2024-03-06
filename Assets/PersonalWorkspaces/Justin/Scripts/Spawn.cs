379079871382dfb767c24add477ebb818b1c67ab
Justin Nguyen <jnguyen2737@student.pps.net>
2024-03-06 09:20:21 -0800

    Spawn Points

--- Assets/PersonalWorkspaces/Justin/Scripts/Spawn.cs
+++ Assets/PersonalWorkspaces/Justin/Scripts/Spawn.cs
@@ -14,12 +14,12 @@ public class Spawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 4; i++)
         {
             
-            spawnPointsA.Add(Instantiate(spawnPoint, new Vector3((-45f + xIncrement), 19.01464f, -2.5f), Quaternion.identity));
-            spawnPointsB.Add(Instantiate(spawnPoint, new Vector3(42f - xIncrement, 19.01464f, 6f), Quaternion.identity));
-            xIncrement += 2f;
+            spawnPointsA.Add(Instantiate(spawnPoint, new Vector3((-314f + xIncrement), 19.01464f, -362f), Quaternion.identity));
+            spawnPointsB.Add(Instantiate(spawnPoint, new Vector3(-221f - xIncrement, 19.01464f, -380f), Quaternion.identity));
+            xIncrement += 3f;
         }
     }
 
