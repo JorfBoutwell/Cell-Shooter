53240bddc4b6eba2b22ffb02c1a98fc5f7edfcb3
Justin Nguyen <jnguyen2737@student.pps.net>
2024-03-06 09:12:57 -0800

    Update Train Scene
    Added Points and WinOverlay

--- Assets/PersonalWorkspaces/Justin/Scripts/Spawn.cs
+++ Assets/PersonalWorkspaces/Justin/Scripts/Spawn.cs
@@ -9,13 +9,17 @@ public class Spawn : MonoBehaviour
     public List<GameObject> spawnPointsA;
     public List<GameObject> spawnPointsB;
 
+    public float xIncrement = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < 5; i++)
         {
-            spawnPointsA.Add(Instantiate(spawnPoint, new Vector3(0, 0, 0), Quaternion.identity));
-            spawnPointsB.Add(Instantiate(spawnPoint, new Vector3(0, 0, 0), Quaternion.identity));
+            
+            spawnPointsA.Add(Instantiate(spawnPoint, new Vector3((-45f + xIncrement), 19.01464f, -2.5f), Quaternion.identity));
+            spawnPointsB.Add(Instantiate(spawnPoint, new Vector3(42f - xIncrement, 19.01464f, 6f), Quaternion.identity));
+            xIncrement += 2f;
         }
     }
 
