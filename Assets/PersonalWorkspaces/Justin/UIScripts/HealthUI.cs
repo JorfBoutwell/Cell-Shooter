02a44b1d840586fff8ecb85c4323d92682c69743
hcannon3370 <128831009+hcannon3370@users.noreply.github.com>
2024-05-06 14:56:08 -0700

    funky hotfix

--- Assets/PersonalWorkspaces/Justin/UIScripts/HealthUI.cs
+++ Assets/PersonalWorkspaces/Justin/UIScripts/HealthUI.cs
@@ -11,8 +11,11 @@ public class HealthUI : MonoBehaviour
     public GameObject healthBar;
     public GameObject healthBarShadow;
     public float healthBarUI = 680f;
+    public float maxWidth = 680f;
     public float healthShadow;
     public float lastHealth = 100;
+
+    RectTransform healthRect;
 
     float damageTaken = 50f; //damageTaken value for testing
     float damageTakenUI;
@@ -20,6 +23,7 @@ public class HealthUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        healthRect = healthBar.GetComponent<RectTransform>();
         healthShadow = healthBarUI;
         damageTakenUI = damageTaken * 6.8f; //6.8 to scale with the UI
     }
@@ -27,6 +31,10 @@ public class HealthUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(healthRect.sizeDelta.x > maxWidth)
+        {
+            healthRect.sizeDelta = new Vector2(maxWidth, 90f);
+        }
         //Health UI Damage
         if (Input.GetKeyDown(KeyCode.L)) //For Testing
         { 
